f9771ad1bd325f9f53fe26e3ae304b4d399bc449
HARISH SEKAR <dsharish@outlook.com>
2020-05-15 09:59:20 +0530

    Edit Employee Form

---
+++ EmployeeManagement.web/Pages/EditEmployeeBase.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.web.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.web.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeService  EmployeeService { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
