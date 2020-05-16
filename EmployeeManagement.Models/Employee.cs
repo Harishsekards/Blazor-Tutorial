9665ded8d3cd595b7e0e80836f142f2e04e6fd08
HARISH SEKAR <dsharish@outlook.com>
2020-05-16 11:23:46 +0530

    Added validation for FirstName,LastName and Email

--- EmployeeManagement.Models/Employee.cs
+++ EmployeeManagement.Models/Employee.cs
@@ -8,12 +8,19 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int EmployeeId { get; set; }
-        [Required]
+
+        [Required(ErrorMessage ="First Name must be provided ")]
+        [MinLength(3,ErrorMessage = "First Name mus be minimum of 3 characters")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage ="Last Name mus be provided")]
+        [MinLength(3, ErrorMessage = "Last Name mus be minimum of 3 characters")]
         public string LastName { get; set; }
+
         [EmailAddress]
         [Required]
         public string Email { get; set; }
+
         public Gender Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public int DepartmentId { get; set; }
